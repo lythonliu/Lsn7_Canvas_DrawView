476e2af4846d71a37699b3dbf46cb6750e18dba1
Lython <liu.junli@moxiangroup.com>
2018-09-16 23:31:59 +0800

    init

---
+++ app/src/main/java/com/dongnao/lsn7_canvas_drawview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.dongnao.lsn7_canvas_drawview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends com.lythonliu.LinkAppCompatActivity {
+    @Override
+    public String getAppName(){
+        return BuildConfig.APP_NAME;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(new DrawView(this));
+    }
+}
